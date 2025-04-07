31504775c4cc06d3306fdbcbcfa0750f41d667f0
Pgalczyn <pgalczynski@student.agh.edu.pl>
2025-04-07 18:42:59 +0200

    lab_3 beggening

---
+++ lab_3/worker.js
@@ -0,0 +1,21 @@
+self.onmessage = function(message) {
+    iterations = message.data;
+    let primes = [];
+    for (let i = 0; i < iterations; i++) {
+        let candidate = i * (1000000000 * Math.random());
+        let isPrime = true;
+        for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+            if (candidate % c === 0) {
+                // not prime
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            primes.push(candidate);
+        }
+    }
+
+    postMessage(primes);
+    self.close();
+}
